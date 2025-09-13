aa5b7bbd2db53a0ca2459818268f0bd0e439f0ba
bdsafta <budi_safta@smktelkom-lpg.sch.id>
2025-09-13 12:26:14 +0700

    Update beberapa kode pada project PHP

--- contact.php
+++ contact.php
@@ -7,7 +7,7 @@ include 'header.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hubungi Kami - Belajar PHP</title>
+    <title>Hubungi Kami - Belajar Backend PHP</title>
     <link rel="stylesheet" href="styles.css">
 </head>
 
