a8a0471320e0b8d2c2bb8f37a6c0cfb09790cddf
bdsafta <budi_safta@smktelkom-lpg.sch.id>
2025-09-13 11:46:52 +0700

    Update beberapa kode pada project PHP

--- about.php
+++ about.php
@@ -7,7 +7,7 @@ include 'header.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tentang Website - Belajar PHP</title>
+    <title>Tentang Website - Belajar Backend PHP </title>
     <link rel="stylesheet" href="styles.css">
 </head>
 
